73404025a19380773d76160d46590a022a52188b
AlanNo1 <37204392+AlanNo1@users.noreply.github.com>
2020-12-03 15:35:38 +0800

    Update 拆分表格.py

--- 拆分表格.py
+++ 拆分表格.py
@@ -1,13 +1,13 @@
 import xlwings as xw
-file_path = '湖北_1_10月-移动个人-1.xlsx'
-sheet_name = '整理后'
+file_path = '需拆分的表格.xlsx'
+sheet_name = '需拆分的Sheet'
 app = xw.App(visible = True, add_book = False)
 workbook = app.books.open(file_path)
 worksheet = workbook.sheets[sheet_name]
 value = worksheet.range('A2').expand('table').value
 data = dict()
 for i in range(len(value)):
-    product_name = value[i][3]
+    product_name = value[i][3]#需要拆分的列，按列名进行拆分表格
     if product_name not in data:
         data[product_name] = []
     data[product_name].append(value[i])
